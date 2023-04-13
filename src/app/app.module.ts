d3326bb3bc9ecae0362edc1682d5c21c72d8a0aa
Anmol <anmolshingari2000@gmail,com>
2023-04-13 13:27:34 +0530

    angularapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmployerComponent } from './components/employer/employer.component';
+import { CandidateDetailsComponent } from './components/candidate-details/candidate-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupUpPageComponent,
+    LoginPageComponent,
+    HomeComponent,
+    EmployerComponent,
+    CandidateDetailsComponent
   ],
   imports: [
     BrowserModule,
